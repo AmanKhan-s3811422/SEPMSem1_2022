5063884080ab32581dd5380be6cac8e117c6b8b6
Zane Van_Gramberg s3729079 <s3729079@student.rmit.edu.au>
2022-04-26 16:03:51 +1000

    PB1.1 - header
    header component created - still needs styling

--- ui/src/components/routing/Pages.js
+++ ui/src/components/routing/Pages.js
@@ -1,10 +1,12 @@
 import {Route, BrowserRouter as Router, Routes} from "react-router-dom";
 import React from "react";
 import HomePage from "../homepage/HomePage";
+import Header from "../Header";
 
 const Pages = () => {
     return (
             <Router>
+                <Header/>
                 <Routes>
                     <Route path="/" element={<HomePage/>}/>
                 </Routes>
