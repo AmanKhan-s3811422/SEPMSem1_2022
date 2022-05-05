9433124409ddd5822d0c08c6aad86cea8262a84e
Zane Van_Gramberg s3729079 <s3729079@student.rmit.edu.au>
2022-05-05 11:33:45 +1000

    All sprint 1 PBs react work ready to test

--- ui/src/GlobalState.js
+++ ui/src/GlobalState.js
@@ -9,162 +9,192 @@ export const DataProvider = ({children}) => {
         {
             id: 0,
             char: '',
+            checked: false,
             isInWord: false,
             isInPosition: false
         },{
             id: 1,
             char: '',
+            checked: false,
             isInWord: false,
             isInPosition: false
         },{
             id: 2,
             char: '',
+            checked: false,
             isInWord: false,
             isInPosition: false
         },{
             id: 3,
             char: '',
+            checked: false,
             isInWord: false,
             isInPosition: false
         },{
             id: 4,
             char: '',
             isInWord: false,
+            checked: false,
             isInPosition: false
         },{
             id: 5,
             char: '',
+            checked: false,
             isInWord: false,
             isInPosition: false
         },{
             id: 6,
             char: '',
+            checked: false,
             isInWord: false,
             isInPosition: false
         },{
             id: 7,
             char: '',
+            checked: false,
             isInWord: false,
             isInPosition: false
         },{
             id: 8,
             char: '',
+            checked: false,
             isInWord: false,
             isInPosition: false
         },{
             id: 9,
             char: '',
+            checked: false,
             isInWord: false,
             isInPosition: false
         },{
             id: 10,
             char: '',
+            checked: false,
             isInWord: false,
             isInPosition: false
         },{
             id: 11,
             char: '',
+            checked: false,
             isInWord: false,
             isInPosition: false
         },{
             id: 12,
             char: '',
+            checked: false,
             isInWord: false,
             isInPosition: false
         },{
             id: 13,
             char: '',
+            checked: false,
             isInWord: false,
             isInPosition: false
         },{
             id: 14,
             char: '',
+            checked: false,
             isInWord: false,
             isInPosition: false
         },{
             id: 15,
             char: '',
+            checked: false,
             isInWord: false,
             isInPosition: false
         },{
             id: 16,
             char: '',
+            checked: false,
             isInWord: false,
             isInPosition: false
         },{
             id: 17,
             char: '',
+            checked: false,
             isInWord: false,
             isInPosition: false
         },{
             id: 18,
             char: '',
+            checked: false,
             isInWord: false,
             isInPosition: false
         },{
             id: 19,
             char: '',
+            checked: false,
             isInWord: false,
             isInPosition: false
         },{
             id: 20,
             char: '',
+            checked: false,
             isInWord: false,
             isInPosition: false
         },{
             id: 21,
             char: '',
+            checked: false,
             isInWord: false,
             isInPosition: false
         },{
             id: 22,
             char: '',
+            checked: false,
             isInWord: false,
             isInPosition: false
         },{
             id: 23,
             char: '',
+            checked: false,
             isInWord: false,
             isInPosition: false
         },{
             id: 24,
             char: '',
+            checked: false,
             isInWord: false,
             isInPosition: false
         },{
             id: 25,
             char: '',
+            checked: false,
             isInWord: false,
             isInPosition: false
         },{
             id: 26,
             char: '',
+            checked: false,
             isInWord: false,
             isInPosition: false
         },{
             id: 27,
             char: '',
+            checked: false,
             isInWord: false,
             isInPosition: false
         },{
             id: 28,
             char: '',
+            checked: false,
             isInWord: false,
             isInPosition: false
         },{
             id: 29,
             char: '',
+            checked: false,
             isInWord: false,
             isInPosition: false
         }
     ]
 
     const [gameBoard, setGameBoard] = useState(tiles)
-    const [gameOver, setGameOver] = useState(false)
+    const [gameFin, setGameFin] = useState(false)
 
     const statedData = {
         gameBoard: [gameBoard, setGameBoard],
-        gameOver: [gameOver, setGameOver],
+        gameFin: [gameFin, setGameFin],
         wordAPI: WordAPI()
     }
 
--- ui/src/components/homepage/Grid.js
+++ ui/src/components/homepage/Grid.js
@@ -9,7 +9,8 @@ const Grid = callback => {
     const [currentWord, setCurrentWord] = useState('')
     const [guess, setGuess] = useState(0)
     const [word] = state.wordAPI.wordOfTheDay
-    const [gameOver, setGameOver] = state.gameOver
+    const [gameFin, setGameFin] = state.gameFin
+    const [gameBoard, setGameBoard] = state.gameBoard
     let refs = useRef([]);
     refs.current = [
         0,0,0,0,0,
@@ -26,23 +27,16 @@ const Grid = callback => {
     }, [currentWord]);
 
     const focusInput = () => {
-        let wl = currentWord.length
-        let line = guess * 5
-        if(wl < 5){
-            refs.current[wl + line].current.focus()
-        } else if (wl === 5){
-            refs.current[wl-1 + line].current.focus()
+        try{
+            let wl = currentWord.length
+            let line = guess * 5
+            if(wl < 5){
+                refs.current[wl + line].current.focus()
+            } else if (wl === 5){
+                refs.current[wl-1 + line].current.focus()
+            }
+        } catch (e) {
         }
-    }
-
-    const blurCallback = () => {
-        focusInput()
-    }
-
-
-    const isTheWord = word => {
-        console.log('isnt the word')
-        return false
     }
 
     const notEnoughLetters = async () => {
@@ -57,8 +51,61 @@ const Grid = callback => {
     const handleGameOver = async () => {
         // disable the board
         console.log("Game over :(")
-        setGameOver(true)
+        setGameFin(true)
         await errorCallback(word, false)
+    }
+
+    const checkLetter = (letter, position) => {
+        let line = guess * 5
+        if (word.includes(letter)){
+            if (word[position] === letter){
+                setGameBoard(initialGameBoard => {
+                    return [
+                        ...initialGameBoard.slice(0, position+line),
+                        // tile,
+                        {...initialGameBoard[position+line], checked: true, isInWord:true, isInPosition:true},
+                        ...initialGameBoard.slice(position+line + 1)
+                    ]
+                })
+            } else {
+                setGameBoard(initialGameBoard => {
+                    return [
+                        ...initialGameBoard.slice(0, position+line),
+                        // tile,
+                        {...initialGameBoard[position+line], checked: true, isInWord:true},
+                        ...initialGameBoard.slice(position+line + 1)
+                    ]
+                })
+            }
+        } else {
+            setGameBoard(initialGameBoard => {
+                return [
+                    ...initialGameBoard.slice(0, position+line),
+                    // tile,
+                    {...initialGameBoard[position+line], checked: true},
+                    ...initialGameBoard.slice(position+line + 1)
+                ]
+            })
+        }
+    }
+
+    const checkWord = async wordToCheck => {
+        // assumes word is a valid 5 letter English word
+        for (let i = 0; i < wordToCheck.length; i++) {
+            checkLetter(wordToCheck[i], i);
+        }
+        if (wordToCheck === word) {
+            console.log('You found the word!')
+            setGameFin(true)
+        } else {
+            // case word is wrong
+            if (guess < 5) {
+                setGuess(guess + 1)
+            } else {
+                await handleGameOver()
+            }
+            setCurrentWord("")
+        }
     }
 
     const nextInput = async value => {
@@ -75,23 +122,11 @@ const Grid = callback => {
                 const response = await axios.get(url).catch(() => {
                     return false
                 })
-                // case word is in list
                 if (response && response.data.length > 0) {
-                    console.log('is a word')
-                    if (isTheWord(currentWord)) {
-                        // case word is correct (game won!)
-                        console.log('You found the word!')
-                    } else {
-                        // case word is wrong
-                        if (guess < 5) {
-                            setGuess(guess + 1)
-                        } else {
-                            await handleGameOver()
-                        }
-                        setCurrentWord("")
-                    }
+                    // case word is in dictionary
+                    await checkWord(currentWord)
                 } else {
-                    // case word is not in the list
+                    // case word is not in dictionary
                     await wordNotInList()
                 }
             }
@@ -104,46 +139,46 @@ const Grid = callback => {
     return (
         <div className={'wordle-grid'}>
             <div className={'wordle-row'}>
-                <LetterBox id={0} tileRef={refs.current[0]} callback={nextInput} blurCallback={blurCallback}/>
-                <LetterBox id={1} tileRef={refs.current[1]} callback={nextInput} blurCallback={blurCallback}/>
-                <LetterBox id={2} tileRef={refs.current[2]} callback={nextInput} blurCallback={blurCallback}/>
-                <LetterBox id={3} tileRef={refs.current[3]} callback={nextInput} blurCallback={blurCallback}/>
-                <LetterBox id={4} tileRef={refs.current[4]} callback={nextInput} blurCallback={blurCallback}/>
+                <LetterBox id={0} tileRef={refs.current[0]} callback={nextInput} blurCallback={focusInput}/>
+                <LetterBox id={1} tileRef={refs.current[1]} callback={nextInput} blurCallback={focusInput}/>
+                <LetterBox id={2} tileRef={refs.current[2]} callback={nextInput} blurCallback={focusInput}/>
+                <LetterBox id={3} tileRef={refs.current[3]} callback={nextInput} blurCallback={focusInput}/>
+                <LetterBox id={4} tileRef={refs.current[4]} callback={nextInput} blurCallback={focusInput}/>
             </div>
             <div className={'wordle-row'}>
-                <LetterBox id={5} tileRef={refs.current[5]} callback={nextInput} blurCallback={blurCallback}/>
-                <LetterBox id={6} tileRef={refs.current[6]} callback={nextInput} blurCallback={blurCallback}/>
-                <LetterBox id={7} tileRef={refs.current[7]} callback={nextInput} blurCallback={blurCallback}/>
-                <LetterBox id={8} tileRef={refs.current[8]} callback={nextInput} blurCallback={blurCallback}/>
-                <LetterBox id={9} tileRef={refs.current[9]} callback={nextInput} blurCallback={blurCallback}/>
+                <LetterBox id={5} tileRef={refs.current[5]} callback={nextInput} blurCallback={focusInput}/>
+                <LetterBox id={6} tileRef={refs.current[6]} callback={nextInput} blurCallback={focusInput}/>
+                <LetterBox id={7} tileRef={refs.current[7]} callback={nextInput} blurCallback={focusInput}/>
+                <LetterBox id={8} tileRef={refs.current[8]} callback={nextInput} blurCallback={focusInput}/>
+                <LetterBox id={9} tileRef={refs.current[9]} callback={nextInput} blurCallback={focusInput}/>
             </div>
             <div className={'wordle-row'}>
-                <LetterBox id={10} tileRef={refs.current[10]} callback={nextInput} blurCallback={blurCallback}/>
-                <LetterBox id={11} tileRef={refs.current[11]} callback={nextInput} blurCallback={blurCallback}/>
-                <LetterBox id={12} tileRef={refs.current[12]} callback={nextInput} blurCallback={blurCallback}/>
-                <LetterBox id={13} tileRef={refs.current[13]} callback={nextInput} blurCallback={blurCallback}/>
-                <LetterBox id={14} tileRef={refs.current[14]} callback={nextInput} blurCallback={blurCallback}/>
+                <LetterBox id={10} tileRef={refs.current[10]} callback={nextInput} blurCallback={focusInput}/>
+                <LetterBox id={11} tileRef={refs.current[11]} callback={nextInput} blurCallback={focusInput}/>
+                <LetterBox id={12} tileRef={refs.current[12]} callback={nextInput} blurCallback={focusInput}/>
+                <LetterBox id={13} tileRef={refs.current[13]} callback={nextInput} blurCallback={focusInput}/>
+                <LetterBox id={14} tileRef={refs.current[14]} callback={nextInput} blurCallback={focusInput}/>
             </div>
             <div className={'wordle-row'}>
-                <LetterBox id={15} tileRef={refs.current[15]} callback={nextInput} blurCallback={blurCallback}/>
-                <LetterBox id={16} tileRef={refs.current[16]} callback={nextInput} blurCallback={blurCallback}/>
-                <LetterBox id={17} tileRef={refs.current[17]} callback={nextInput} blurCallback={blurCallback}/>
-                <LetterBox id={18} tileRef={refs.current[18]} callback={nextInput} blurCallback={blurCallback}/>
-                <LetterBox id={19} tileRef={refs.current[19]} callback={nextInput} blurCallback={blurCallback}/>
+                <LetterBox id={15} tileRef={refs.current[15]} callback={nextInput} blurCallback={focusInput}/>
+                <LetterBox id={16} tileRef={refs.current[16]} callback={nextInput} blurCallback={focusInput}/>
+                <LetterBox id={17} tileRef={refs.current[17]} callback={nextInput} blurCallback={focusInput}/>
+                <LetterBox id={18} tileRef={refs.current[18]} callback={nextInput} blurCallback={focusInput}/>
+                <LetterBox id={19} tileRef={refs.current[19]} callback={nextInput} blurCallback={focusInput}/>
             </div>
             <div className={'wordle-row'}>
-                <LetterBox id={20} tileRef={refs.current[20]} callback={nextInput} blurCallback={blurCallback}/>
-                <LetterBox id={21} tileRef={refs.current[21]} callback={nextInput} blurCallback={blurCallback}/>
-                <LetterBox id={22} tileRef={refs.current[22]} callback={nextInput} blurCallback={blurCallback}/>
-                <LetterBox id={23} tileRef={refs.current[23]} callback={nextInput} blurCallback={blurCallback}/>
-                <LetterBox id={24} tileRef={refs.current[24]} callback={nextInput} blurCallback={blurCallback}/>
+                <LetterBox id={20} tileRef={refs.current[20]} callback={nextInput} blurCallback={focusInput}/>
+                <LetterBox id={21} tileRef={refs.current[21]} callback={nextInput} blurCallback={focusInput}/>
+                <LetterBox id={22} tileRef={refs.current[22]} callback={nextInput} blurCallback={focusInput}/>
+                <LetterBox id={23} tileRef={refs.current[23]} callback={nextInput} blurCallback={focusInput}/>
+                <LetterBox id={24} tileRef={refs.current[24]} callback={nextInput} blurCallback={focusInput}/>
             </div>
             <div className={'wordle-row'}>
-                <LetterBox id={25} tileRef={refs.current[25]} callback={nextInput} blurCallback={blurCallback}/>
-                <LetterBox id={26} tileRef={refs.current[26]} callback={nextInput} blurCallback={blurCallback}/>
-                <LetterBox id={27} tileRef={refs.current[27]} callback={nextInput} blurCallback={blurCallback}/>
-                <LetterBox id={28} tileRef={refs.current[28]} callback={nextInput} blurCallback={blurCallback}/>
-                <LetterBox id={29} tileRef={refs.current[29]} callback={nextInput} blurCallback={blurCallback}/>
+                <LetterBox id={25} tileRef={refs.current[25]} callback={nextInput} blurCallback={focusInput}/>
+                <LetterBox id={26} tileRef={refs.current[26]} callback={nextInput} blurCallback={focusInput}/>
+                <LetterBox id={27} tileRef={refs.current[27]} callback={nextInput} blurCallback={focusInput}/>
+                <LetterBox id={28} tileRef={refs.current[28]} callback={nextInput} blurCallback={focusInput}/>
+                <LetterBox id={29} tileRef={refs.current[29]} callback={nextInput} blurCallback={focusInput}/>
             </div>
         </div>
     );
