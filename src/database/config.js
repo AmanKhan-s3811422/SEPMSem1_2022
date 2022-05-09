009769f1a0250b236385d6508888eb4ce618bc24
[Sumeet Sandhu s3826391] <s3826391@student.rmit.edu.au>
2022-05-09 21:37:46 +1000

    changes

--- src/database/config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    HOST: "database-1.cqhoamyiuejj.ap-southeast-2.rds.amazonaws.com",
-    PORT: "3306",
-    USER: "admin",
-    PASSWORD: "ssKi8coSqAm91ABVUSVX",
-    DB: "mariadb",
-    DIALECT: "mariadb",
-  };
-  
